30b6604983622c8047fa29d903c82e401efdb8f4
Thomas Sanders <t.sanders@televic.com>
2017-07-07 10:59:36 +0200

    Improvement for disabled checkbox / radiobutton

--- components/checkbox/checkbox.ts
+++ components/checkbox/checkbox.ts
@@ -21,7 +21,7 @@ export const CHECKBOX_VALUE_ACCESSOR: any = {
                 <span class="ui-chkbox-icon ui-c" [ngClass]="{'fa fa-check':checked}"></span>
             </div>
         </div>
-        <label class="ui-chkbox-label" (click)="onClick($event,cb,true)" *ngIf="label">{{label}}</label>
+        <label class="ui-chkbox-label" (click)="onClick($event,cb,true)" *ngIf="label" [ngClass]="{'disabled':disabled}">{{label}}</label>
     `,
     providers: [CHECKBOX_VALUE_ACCESSOR]
 })
